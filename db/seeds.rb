4a5eb3e2025ec2ef960d8fd1203dbb1c0ad1571e
sapir alagem <sapir.alagem@gmail.com>
2020-01-11 12:04:10 +0000

    Add prouct hunt list

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+puts 'Creating 5 products...'
+5.times do |i|
+  product = Product.create!(
+    name: Faker::Company.name,
+    tagline: Faker::Company.catch_phrase
+  )
+  puts "#{i + 1}. #{product.name}"
+end
+puts 'Finished!'
